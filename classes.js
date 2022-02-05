78bb3943feae696c1e05c3978ec488fdae6824a1
yughias <75031586+yughias@users.noreply.github.com>
2022-02-05 18:15:35 +0100

    Add files via upload

--- classes.js
+++ classes.js
@@ -50,7 +50,7 @@ class Player{
 
   update(){
     this.angle = atan2(mouseY-windowHeight/2, mouseX-windowWidth/2);
-    if(mouseIsPressed && mouseButton == LEFT){
+    if(mouseIsPressed){
       this.vx += this.acc*cos(this.angle)*(deltaTime/1000.0);
       this.vy += this.acc*sin(this.angle)*(deltaTime/1000.0);
       if(this.vx > this.vlimit)
@@ -106,68 +106,64 @@ class Bar{
         circle(this.padding+this.x+this.w-this.h/2, this.y+space+this.h/2, 10);
         space += 20;
       }
-      if(this.clicked){
+      fill(0);
+      stroke(0);
+      rect(this.x, this.y, this.padding+this.w+1, min(this.size+1, this.frame));
+      stroke(255);
+      fill(255);
+      if(this.frame > this.size){
         fill(0);
         stroke(0);
-        rect(this.x, this.y, this.padding+this.w+1, min(this.size+1, this.frame));
-        stroke(255);
-        fill(255);
-        if(this.frame > this.size){
-          fill(0);
-          stroke(0);
-          rect(windowWidth/scaleWin, 0, this.size-this.frame, this.size);
-
-          if(mouseY < this.size*scaleWin)
-            if(mouseX > width/2-this.github.width*scaleWin/2 && mouseX < width/2+this.github.width*scaleWin/2)
-              tint(255-this.hover, 255-this.hover*1.5, 255-this.hover*1.25);
-          image(this.github, windowWidth/scaleWin/2, this.size/2);
-          noTint();
+        rect(windowWidth/scaleWin, 0, this.size-this.frame, this.size);
 
         if(mouseY < this.size*scaleWin)
-            if(mouseX > width/4-this.youtube.width*scaleWin/2 && mouseX < width/4+this.youtube.width*scaleWin/2)
-              tint(255, 255-this.hover*1.5, 255-this.hover*1.5);
-          image(this.youtube, windowWidth/scaleWin/4, this.size/2);
-          noTint();
+          if(mouseX > width/2-this.github.width*scaleWin/2 && mouseX < width/2+this.github.width*scaleWin/2)
+            tint(255-this.hover, 255-this.hover*1.5, 255-this.hover*1.25);
+        image(this.github, windowWidth/scaleWin/2, this.size/2);
+        noTint();
 
-          if(mouseY < this.size*scaleWin)
-            if(mouseX > 3*width/4-this.discord.width*scaleWin/2 && mouseX < 3*width/4+this.discord.width*scaleWin/2)
-              tint(255-this.over, 255-this.hover, 255);
-          image(this.discord, 3*(windowWidth/scaleWin)/4, this.size/2);
-          noTint();
+      if(mouseY < this.size*scaleWin)
+          if(mouseX > width/4-this.youtube.width*scaleWin/2 && mouseX < width/4+this.youtube.width*scaleWin/2)
+            tint(255, 255-this.hover*1.5, 255-this.hover*1.5);
+        image(this.youtube, windowWidth/scaleWin/4, this.size/2);
+        noTint();
 
-          rect(0, 0, windowWidth/scaleWin-this.frame+this.size, this.size);
-          fill(255);
-          stroke(255);
-          line(windowWidth/scaleWin, this.size, (windowWidth/scaleWin+this.size-this.frame), this.size);
-        }
+        if(mouseY < this.size*scaleWin)
+          if(mouseX > 3*width/4-this.discord.width*scaleWin/2 && mouseX < 3*width/4+this.discord.width*scaleWin/2)
+            tint(255-this.over, 255-this.hover, 255);
+        image(this.discord, 3*(windowWidth/scaleWin)/4, this.size/2);
+        noTint();
+
+        rect(0, 0, windowWidth/scaleWin-this.frame+this.size, this.size);
+        fill(255);
+        stroke(255);
+        line(windowWidth/scaleWin, this.size, (windowWidth/scaleWin+this.size-this.frame), this.size);
       }
   }
 
   update(){
+    let sign = (this.clicked)? 1 : -1;
     let hovering = false;
-    if(this.clickable && mouseIsPressed && mouseX >= 0 && mouseY >= 0 && mouseX <= (this.padding+this.w)*scaleWin && mouseY <= this.size*scaleWin)
-      {
-        this.clicked = !this.clicked;
-        this.clickable = false;
-      }
-    if(this.clicked){
-      if(this.frame < this.size)
-        this.frame += deltaTime/10;
-      else {
-        if(windowWidth/scaleWin+this.size-this.frame > 0)
-          this.frame += this.frame*(deltaTime/200);
-        else{
-          if(mouseY < this.size*scaleWin){
-            if(mouseX > width/4-this.youtube.width*scaleWin/2 && mouseX < width/4+this.youtube.width*scaleWin/2)
-              hovering = true;
-            else if(mouseX > width/2-this.github.width*scaleWin/2 && mouseX < width/2+this.github.width*scaleWin/2)
-              hovering = true;
-            else if(mouseX > 3*width/4-this.discord.width*scaleWin/2 && mouseX < 3*width/4+this.discord.width*scaleWin/2)
-              hovering = true;
-          }
+
+    if(this.frame == 0)
+      this.clickable = true;
+    if(mouseY > this.size*scaleWin && this.clicked && mouseIsPressed)
+      this.clicked = false;
+    if(this.frame < this.size)
+      this.frame = max(this.frame+sign*deltaTime/10, 0);
+    else {
+      this.frame = min(windowWidth/scaleWin+this.size, this.frame+sign*this.frame*(deltaTime/200)) ;
+      if(this.frame == windowWidth/scaleWin+this.size)
+        if(mouseY < this.size*scaleWin){
+          if(mouseX > width/4-this.youtube.width*scaleWin/2 && mouseX < width/4+this.youtube.width*scaleWin/2)
+            hovering = true;
+          else if(mouseX > width/2-this.github.width*scaleWin/2 && mouseX < width/2+this.github.width*scaleWin/2)
+            hovering = true;
+          else if(mouseX > 3*width/4-this.discord.width*scaleWin/2 && mouseX < 3*width/4+this.discord.width*scaleWin/2)
+            hovering = true;
         }
-      }
     }
+
     if(!hovering){
       cursor(ARROW);
       this.hover = 0;
@@ -175,9 +171,16 @@ class Bar{
       this.hover = min(this.hover+deltaTime/5, 255/2);
       cursor(HAND);
     }
+    if(this.clickable && mouseX >= 0 && mouseY >= 0 && mouseX <= (this.padding+this.w)*scaleWin && mouseY <= this.size*scaleWin)
+      cursor(HAND);
   }
 
   checkClick(){
+    if(this.clickable && mouseX >= 0 && mouseY >= 0 && mouseX <= (this.padding+this.w)*scaleWin && mouseY <= this.size*scaleWin){
+      this.clicked = true;
+      this.clickable = false;
+    }
+
     if(this.hover != 0 && mouseY < this.size){
       if(mouseX > width/4-this.youtube.width*scaleWin/2 && mouseX < width/4+this.youtube.width*scaleWin/2)
         window.open("https://www.youtube.com/channel/UC3hAN4vRIoGZDNPUwmEDZ2Q");
