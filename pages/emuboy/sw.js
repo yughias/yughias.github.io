17268f7002037b4df29f4c9c78764d0dd96e975e
sasal <sasa.laporta@gmail.com>
2024-06-11 21:37:24 +0200

    - joypad fixed for emuboy and emuboy GX - pwa test for emuboy

---
+++ pages/emuboy/sw.js
@@ -0,0 +1,20 @@
+var cacheName = 'emuboy';
+var filesToCache = [
+    '*',
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
