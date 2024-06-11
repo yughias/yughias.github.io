cb6b8e4739c8f8c0e5934b02f3a673bcda77a86a
sasal <sasa.laporta@gmail.com>
2024-06-11 22:17:06 +0200

    fixed pwa attempt 1

--- pages/emuboy/sw.js
+++ pages/emuboy/sw.js
@@ -1,20 +1,56 @@
-var cacheName = 'emuboy';
-var filesToCache = [
-    '*',
-];
+const CACHE_NAME = 'emuboy';
 
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+        '/',
+
+        '/img/cgb-cartridge.jpg',
+        '/img/cgb-icon.png',
+        '/img/cgb-overlay.png',
+        '/img/dmg-cartridge.png',
+        '/img/dmg-icon.png',
+        '/img/dmg-overlay.png',
+        '/img/icon.png',
+        '/img/icon512_maskable.png',
+        '/img/icon512_rounded.png',
+        '/img/megaduck-cartridge.png',
+        '/img/megaduck-overlay.png',
+      
+        '/style/dmg.css',
+        'style/cgb.css',
+        'style/megaduck.css',
+
+        '/emulator.html',
+        '/actions.js',
+        '/emulator.js',
+        '/sw.js',
+        '/emulator.wasm',
+    ]);
+  })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // Get the resource from the cache.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
+
+          // Save the resource in the cache and return it.
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed.
+        }
+    }
+  })());
 });
